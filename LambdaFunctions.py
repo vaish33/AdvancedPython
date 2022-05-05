bbd3e91f589ebf0e386244bd30079b78aab46645
vaish33 <vaishnaviis@outlook.com>
2022-05-05 17:19:27 +0530

    updated Lambda Functions

---
+++ LambdaFunctions.py
@@ -0,0 +1,19 @@
+#Python Lambda Functions are anonymous function means that the function is without a name.
+#Python Lambda Function Syntax is lambda arguments: expression
+#This function can have any number of arguments but only one expression, which is evaluated and returned.
+#lambda functions can be used wherever function objects are required.
+# lambda functions are restricted to a single expression.
+
+# Python program to demonstrate
+# lambda functions
+
+
+x ="WelcometoIndia"
+
+# lambda gets pass to print
+(lambda x : print(x))(x)
+
+#Output
+WelcometoIndia
+
+
